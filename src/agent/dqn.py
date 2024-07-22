c8c949b4a2598a4a5bda81155ae6f10163646177
Yannis Liu <liuyiyao0916@163.com>
2024-07-22 14:33:26 +0800

    Update dqn.py

--- src/agent/dqn.py
+++ src/agent/dqn.py
@@ -1,217 +1,71 @@
-                for _images, _label, _name in tqdm(self.train_loader):
-                    # print(index)
-                    if next_images is not None:
-                        cur_images = next_images
-                        cur_label = next_label
-                        cur_name = next_name
-                        if index == (len(self.train_loader) - 1):
-                            next_images, next_label, next_name = next(data_iter)
-                        else:
-                            next_images = _images
-                            next_label = _label
-                            next_name = _name
-                    else:
-                        cur_images = _images
-                        cur_label = _label
-                        cur_name = _name
-                        next_images, next_label, next_name = next(data_iter)
-                    images = cur_images.to(self.device)
-                    label = cur_label.to(self.device)
-                    name = cur_name
-                    index += 1
-                    cam_extractor = XGradCAM(self.unet, ['module.fire9_1', 'module.fire9_2'])
-                    # cam_extractor_e = XGradCAM(self.unet, 'module.e_conv5_x')
-                    cam_extractor._hooks_enabled = True
-                    # cam_extractor_e._hooks_enabled = True
-                    activation_map_g = torch.ones((images.shape[0], 32, 32)).to(self.device)
-                    activation_map_e = torch.ones((images.shape[0], 32, 32)).to(self.device)
+import torch
+import torch.nn as nn
+import torch.autograd as autograd
+import numpy as np
 
-                    if self.modal == 0:
-                        cls = self.unet(images[:, 0:3, :, :])
-                    elif self.modal == 1:
-                        cls = self.unet(images[:, 3:6, :, :])
-                    elif self.modal == 2:
-                        cls = self.unet(images[:, 0:6, :, :])
-                    elif self.modal == 4:
-                        if epoch < self.warmup:
-                            cls, g_att_output, e_att_output, x1_1, x2_1, affin_grid_1, affin_grid_2, edge, color = self.unet(
-                                torch.zeros((1)), images[:, 0:3, :, :], images[:, 3:6, :, :],
-                                activation_map_g,
-                                activation_map_e)
-                        else:
-                            cls, g_att_output, e_att_output, x1_1, x2_1, affin_grid_1, affin_grid_2, edge, color = self.unet(
-                                torch.zeros((1)), images[:, 0:3, :, :], images[:, 3:6, :, :],
-                                activation_map_g,
-                                activation_map_e)
-                        # print('*******finish')
+from arch.replay_buffers import BasicBuffer
+from .dqn_model import ConvDQN, DQN
 
-                    activation_map = cam_extractor(cls.argmax(axis=1).squeeze(0).tolist(), cls)
-                    # activation_map_e = cam_extractor_e(cls.argmax(axis=1).squeeze(0).tolist(), cls)
-                    # plt.imshow(activation_map1[0][0].squeeze(0).cpu().numpy())
-                    # plt.axis('off')
-                    # plt.tight_layout()
-                    # plt.show()
 
-                    cam_extractor.remove_hooks()
-                    cam_extractor._hooks_enabled = False
-                    # cam_extractor_e.remove_hooks()
-                    # cam_extractor_e._hooks_enabled = False
+class DQNAgent:
 
-                    # print(np.array(activation_map).shape)
-                    if self.modal == 4:
-                        if epoch < self.warmup:
-                            cls, g_att_output, e_att_output, x1_1, x2_1, affin_grid_1, affin_grid_2, edge, color = self.unet(
-                                torch.zeros((1)), images[:, 0:3, :, :], images[:, 3:6, :, :],
-                                activation_map[0],
-                                activation_map[1])
-                        else:
-                            cls, g_att_output, e_att_output, x1_1, x2_1, affin_grid_1, affin_grid_2, edge, color = self.unet(
-                                torch.ones((1)), images[:, 0:3, :, :], images[:, 3:6, :, :],
-                                activation_map[0],
-                                activation_map[1])
-                    cls_cpu = cls.data.cpu().numpy()
-                    label_cpu = label.data.cpu().numpy()
-                    # print('cls_cpu[:,1]:',cls_cpu[:,1].shape)
-                    # print('np.sum(label_cpu==0):',np.array(np.sum(label_cpu==0)).unsqueeze(0).shape)
-                    state = np.append(cls_cpu[:, 1],
-                                      [np.array(np.sum(label_cpu == 0)), np.array(np.sum(label_cpu == 1))])
-                    # print('label_1:',label_cpu)
+    def __init__(self,  use_conv=False, learning_rate=3e-4, gamma=0.99, buffer_size=10000):
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.replay_buffer = BasicBuffer(max_size=buffer_size)
 
-                    _action1, _action2 = self.dqn.get_action(state)
-                    if self.alpha[0, 0] <= 0.1:
-                        if _action1 < 2:
-                            _action1 = 2
-                    if self.alpha[0, 0] >= 0.9:
-                        if _action1 > 2:
-                            _action1 = 2
-                    if self.cls_criterion.gamma <= 0.1:
-                        if _action2 < 2:
-                            _action2 = 2
-                    if self.cls_criterion.gamma >= 3.9:
-                        if _action2 > 2:
-                            _action2 = 2
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-                    if epoch > self.warmup:
-                        self.alpha[0, 0] += self.action1[_action1]
-                        self.alpha[1, 0] -= self.action1[_action1]
-                        print('action1:', self.action1[_action1], 'action1:', -self.action1[_action1], 'action2:',
-                              self.action1[_action2])
+        self.model = DQN(6,5).to(self.device)
 
-                        self.cls_criterion.alpha = self.alpha
-                        self.cls_criterion.gamma = self.cls_criterion.gamma + self.action1[_action2]
+        self.optimizer = torch.optim.Adam(self.model.parameters(),lr=0.00001)
+        self.MSE_loss = nn.MSELoss()
 
-                    cls_loss = self.cls_criterion(cls, label.long())
-                    loss = cls_loss
-                    epoch_loss += loss.item()
-                    # Backprop + optimize
-                    self.reset_grad()
-                    loss.backward()
-                    self.optimizer.step()
+    def get_action(self, state, eps=0.40):
+        state = torch.FloatTensor(state).float().unsqueeze(0).to(self.device)
+        qvals1, qvals2 = self.model.forward(state)
+        action1 = np.argmax(qvals1.cpu().detach().numpy())
+        action2 = np.argmax(qvals2.cpu().detach().numpy())
+        if(np.random.randn() < eps):
+            action1=np.random.randint(0, 4)
+        if(np.random.randn() < eps):
+            action2=np.random.randint(0, 4)
 
-                    batch_size = label.size(0)
+        return action1, action2
 
-                    total_b = 0
-                    true_b = 0
-                    tp_b = 0
-                    fp_b = 0
-                    tn_b = 0
-                    fn_b = 0
-                    for b in range(batch_size):
-                        total_b += 1
-                        cls_out = cls_cpu[b].argmax()
-                        label_out = label_cpu[b]
-                        if cls_out == label_out:
-                            true_b += 1
-                            if cls_out == 1:
-                                tp_b += 1
-                            else:
-                                tn_b += 1
-                        else:
-                            if cls_out == 1:
-                                fp_b += 1
-                            else:
-                                fn_b += 1
+    def compute_loss(self, batch):
+        states, actions, rewards, next_states, dones = batch
+        states = torch.FloatTensor(states).to(self.device)
+        actions = np.array(actions)
 
-                    cls_acc_b = true_b / total_b
-                    cls_pre_b = tp_b / (tp_b + fp_b + 1e-8)
-                    cls_rec_b = tp_b / (tp_b + fn_b + 1e-8)
-                    cls_spe_b = tn / (tn_b + fp_b + 1e-8)
-                    cls_f1_b = (2 * cls_pre_b * cls_rec_b) / (cls_pre_b + cls_rec_b + 1e-8)
-                    images = next_images.to(self.device)
-                    label = next_label.to(self.device)
-                    # print(images.shape)
-                    # print(label.shape)
-                    activation_map_g = torch.ones((images.shape[0], 32, 32)).to(self.device)
-                    activation_map_e = torch.ones((images.shape[0], 32, 32)).to(self.device)
-                    if self.modal == 4:
-                        if epoch < self.warmup:
-                            cls, g_att_output, e_att_output, x1_1, x2_1, affin_grid_1, affin_grid_2, edge, color = self.unet(
-                                torch.zeros((1)), images[:, 0:3, :, :], images[:, 3:6, :, :],
-                                activation_map_g,
-                                activation_map_e)
-                        else:
-                            cls, g_att_output, e_att_output, x1_1, x2_1, affin_grid_1, affin_grid_2, edge, color = self.unet(
-                                torch.zeros((1)), images[:, 0:3, :, :], images[:, 3:6, :, :],
-                                activation_map_g,
-                                activation_map_e)
-                    # print(cls.shape)
-                    activation_map = cam_extractor(cls.argmax(axis=1).squeeze(0).tolist(), cls)
-                    # activation_map_e = cam_extractor_e(cls.argmax(axis=1).squeeze(0).tolist(), cls)
-                    # plt.imshow(activation_map1[0][0].squeeze(0).cpu().numpy())
-                    # plt.axis('off')
-                    # plt.tight_layout()
-                    # plt.show()
+        actions1, actions2 = actions[:, 0], actions[:, 1]
+        actions1 = torch.LongTensor(actions1).to(self.device)
+        actions2 = torch.LongTensor(actions2).to(self.device)
+        rewards = torch.FloatTensor(rewards).to(self.device)
+        next_states = torch.FloatTensor(next_states).to(self.device)
+        dones = torch.FloatTensor(dones)
 
-                    cam_extractor.remove_hooks()
-                    cam_extractor._hooks_enabled = False
+        curr_Q1, curr_Q2 = self.model.forward(states)
+        curr_Q1 = curr_Q1.gather(1, actions1.unsqueeze(1)).squeeze(1)
+        curr_Q2 = curr_Q2.gather(1, actions2.unsqueeze(1)).squeeze(1)
 
-                    if self.modal == 4:
-                        if epoch < self.warmup:
-                            cls, g_att_output, e_att_output, x1_1, x2_1, affin_grid_1, affin_grid_2, edge, color = self.unet(
-                                torch.zeros((1)), images[:, 0:3, :, :], images[:, 3:6, :, :],
-                                activation_map[0],
-                                activation_map[1])
-                        else:
-                            cls, g_att_output, e_att_output, x1_1, x2_1, affin_grid_1, affin_grid_2, edge, color = self.unet(
-                                torch.ones((1)), images[:, 0:3, :, :], images[:, 3:6, :, :],
-                                activation_map[0],
-                                activation_map[1])
+        next_Q1, next_Q2 = self.model.forward(next_states)
+        max_next_Q1 = torch.max(next_Q1, 1)[0]
+        max_next_Q2 = torch.max(next_Q2, 1)[0]
 
-                    cls_cpu = cls.data.cpu().numpy()
-                    label_cpu = label.data.cpu().numpy()
-                    # print('cls_cpu[:,1]:',cls_cpu[:,1].shape)
-                    # print('np.sum(label_cpu==0):',np.sum(label_cpu==0).shape)
-                    # cls_f1_b*cls_f1_b*cls_f1_b+(cls_rec_b+cls_spe_b-1)*(cls_rec_b+cls_spe_b-1)
-                    next_state = np.append(cls_cpu[:, 1],
-                                           [np.array(np.sum(label_cpu == 0)), np.array(np.sum(label_cpu == 1))])
-                    self.dqn.replay_buffer.push(state, [_action1, _action2], (true_b + tn_b), next_state, 1)
-                    if len(self.dqn.replay_buffer) > 8:
-                        self.dqn.update(8)
-                    # cls_out = cls_cpu.argmax()
-                    for b in range(batch_size):
-                        total += 1
+        expected_Q1 = rewards.squeeze(1) + self.gamma * max_next_Q1
+        expected_Q2 = rewards.squeeze(1) + self.gamma * max_next_Q2
 
-                        name_list.append(name[b])
-                        probility_list.append(cls_cpu[b, 1])
-                        cls_out = cls_cpu[b].argmax()
-                        """if np.exp(cls_cpu[b, 1]) > 0.5:
-                            cls_out = 1
-                        else:
-                            cls_out = 0"""
-                        label_out = label_cpu[b]
-                        label_list.append(label_out)
-                        predict_list.append(cls_out)
-                        if cls_out == label_out:
-                            true += 1
-                            if cls_out == 1:
-                                tp += 1
-                            else:
-                                tn += 1
-                        else:
-                            if cls_out == 1:
-                                fp += 1
-                            else:
-                                fn += 1
+        loss1 = self.MSE_loss(curr_Q1, expected_Q1)
+        loss2 = self.MSE_loss(curr_Q2, expected_Q2)
 
-                # self.scheduler.step(epoch_loss)
-                self.scheduler.step()
+        loss = loss1 + loss2
+        return loss
+
+    def update(self, batch_size):
+        batch = self.replay_buffer.sample(batch_size)
+        loss = self.compute_loss(batch)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
